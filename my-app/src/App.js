01cfdcca1a6f366d9be09b81c1c909cbd1005f39
UlianaOr <Ouliana9@gmail.com>
2022-02-01 17:30:59 +0100

    qwe3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,25 @@
-
-
 function App() {
-  return <div className='App'>Hello</div>
+  return <div className='wrapper'>
+    <header>
+      <div className="headerLeft">
+      <svg />
+      <div className="headerInfo">
+        <h3> React Sneakers</h3>
+        <p>Магазин лучших кроссовок</p>
+      </div>
+      </div>
+      <ul className="headerRight">
+        <li>
+          <svg />
+          <span>1205 руб.</span>
+        </li>
+        <li>
+          <svg />
+        </li>
+      </ul>
+    </header>
+    
+  </div>
 }
 
 export default App;
