fc175a81a73de96aa84d411edb74cc810401c918
UlianaOr <Ouliana9@gmail.com>
2022-02-14 17:03:56 +0100

    qwe11111

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
       <div className="drawer">
         <h2>Корзина</h2>
         <div className="cartItem">
-          <img src="/img/sneakers/1.jpg" alt="Sneakers"/>
+          <img width={70} height={70} src="/img/sneakers/1.jpg" alt="Sneakers"/>
           <div>
             <p>Мужские Кроссовки Nike Air Max 270</p>
             <b>12 999 руб.</b>
