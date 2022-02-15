002682a7345aa252fc3fedbbf1bb042447e698ba
UlianaOr <Ouliana9@gmail.com>
2022-02-15 15:32:06 +0100

    qwq112

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,6 +12,16 @@ function App() {
           </div>
           <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
         </div>
+
+        <div className="cartItem">
+          <img width={70} height={70} src="/img/sneakers/1.jpg" alt="Sneakers"/>
+
+          <div>
+            <p>Мужские Кроссовки Nike Air Max 270</p>
+            <b>12 999 руб.</b>
+          </div>
+          <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
+        </div>
         
       </div>
     </div>
