7437bb13459d18193a8a9a9999f2e774d2f0b69d
UlianaOr <Ouliana9@gmail.com>
2022-02-16 13:07:18 +0100

    qweqwe

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import Card from './components/Card';
+import Header from './components/Heder';
 
 function App() {
   return <div className='wrapper'>
@@ -54,26 +55,7 @@ function App() {
     </div>
 
 
-    <header>
-      <div className="headerLeft">
-      <img width={40} height={40} src="/img/logo.png"/>
-      <div className="headerInfo">
-        <h3> React Sneakers</h3>
-        <p>Магазин лучших кроссовок</p>
-      </div>
-      </div>
-      <ul className="headerRight">
-        <li>
-          <img width={18} height={18} src="/img/car.png"/>
-          <span>
-            1205 руб.
-            </span>
-        </li>
-        <li>
-        <img width={20} height={20} src="/img/user.png"/>
-        </li>
-      </ul>
-    </header>
+    <Header />
 
   <div className="content">
       <h1>Все кросовки</h1>
